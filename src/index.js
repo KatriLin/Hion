166205519b1b47876aba3f4ceb75dfc92a4a1218
KatriLindvall1 <lindvallkatri@hotmail.com>
2024-11-05 11:07:18 +0200

    installed express and defined port

--- src/index.js
+++ src/index.js
@@ -2,6 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
